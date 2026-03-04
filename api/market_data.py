986d80d2e2182c2d464252a0cdd9fb9bc8dcca01
guima <joaopedroguima1307@gmail.com>
2026-03-04 10:22:04 -0300

    refactor: organize data search with a propoer class

--- api/market_data.py
+++ api/market_data.py
@@ -1,11 +1,27 @@
-#https://lucidarium.com.br/como-coletar-dados-de-acoes-e-cripto-com-yfinance-passo-a-passo/
-
 import yfinance as yf
 import pandas as pd 
 
-lista_tickers = ["AAPL", "GOOGL", "MSFT"]
 
+class DataFetcher:
+    def __init__(self):
+        self.lista_tickers = ["AAPL", "GOOGL", "MSFT"]
+        self.period = "5d"
+        self.interval = "1d"
 
-dados_multiplos = yf.download(lista_tickers, period="5d", interval="1d")
+    def get_symbols(self):
+        return self.lista_tickers
+    
+    def set_interval(self, period=None, interval=None):
+        period = period or self.period
+        interval = interval or self.interval
 
-print(dados_multiplos['Close'].head())
+        dados = yf.download(self.lista_tickers, period=period, interval=interval)
+        return dados
+    
+    def get_data(self):
+        dados = self.set_interval()
+        print(dados['Close'].head())
+        return dados
+    
+fetcher = DataFetcher()
+fetcher.get_data()
